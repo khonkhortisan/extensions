a2e5cfee005c1d1809a242bee7c5455182890342
Kyle Mills <Khonkhortisan@gmail.com>
2021-10-15 15:53:28 -0700

    status may be failing to set twice

--- src/es/NovelasLigerasnet.lua
+++ src/es/NovelasLigerasnet.lua
@@ -156,16 +156,17 @@ return {
 		--	ONGOING = NovelStatus.PUBLISHING,
 		--	COMPLETED = NovelStatus.COMPLETED,
 		--})[s] or NovelStatus.UNKNOWN
-
+		
 		local text = function(v) return v:text() end
+		status  =   page:selectFirst(".woocommerce-product-attributes-item--attribute_pa_estado     td p a") and page:selectFirst(".woocommerce-product-attributes-item--attribute_pa_estado     td p a"):text() or ""
+		status = NovelStatus(status == "Completado" and 1 or status == "Pausado" and 2 or status == "En Proceso" and 0 or 3)
 		local novel = NovelInfo {
 			--imageURL = header:selectFirst("img"):attr("src"),
 			imageURL = page:selectFirst(".wp-post-image"):attr("src") or page:selectFirst(".wp-post-image"):attr("srcset"):match("^([^\s]+)"),
 			title = title:text(),
 			authors = { page:selectFirst(".woocommerce-product-attributes-item--attribute_pa_escritor   td p a") and page:selectFirst(".woocommerce-product-attributes-item--attribute_pa_escritor   td p a"):text() or ""},
 			artists = { page:selectFirst(".woocommerce-product-attributes-item--attribute_pa_ilustrador td p a") and page:selectFirst(".woocommerce-product-attributes-item--attribute_pa_ilustrador td p a"):text() or ""},
-			status  =   page:selectFirst(".woocommerce-product-attributes-item--attribute_pa_estado     td p a") and page:selectFirst(".woocommerce-product-attributes-item--attribute_pa_estado     td p a"):text() or "",
-			status = NovelStatus(status == "Completado" and 1 or status == "Pausado" and 2 or status == "En Proceso" and 0 or 3),
+			status = status,
 			genres = map(genres:select("a"), text),
 			tags = map(tags:select("a"), text),
 			description = page:selectFirst(".woocommerce-product-details__short-description"):text(),
