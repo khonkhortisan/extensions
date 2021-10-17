2cf1a43b824bc721cc4cc4aa580c7bd1745b8778
Kyle Mills <Khonkhortisan@gmail.com>
2021-10-16 23:14:56 -0700

    spaces

--- src/en/ReadNovelFull.lua
+++ src/en/ReadNovelFull.lua
@@ -5,11 +5,11 @@ return Require("NovelFull")("https://readnovelfull.com", {
 	name = "ReadNovelFull",
 	imageURL = "https://github.com/shosetsuorg/extensions/raw/dev/icons/ReadNovelFull.png",
 	
-	alternativeTitles =         (map(elem:get(meta_offset    ):select("a"), text)),
-	authors           =         (map(elem:get(meta_offset + 1):select("a"), text)),
-	genres            =         (map(elem:get(meta_offset + 2):select("a"), text)),
-	--sources         =         (map(elem:get(meta_offset + 3):select("a"), text)),
-	status            = (NovelStatus(elem:get(meta_offset + 4):select("a"):text() == "Completed" and 1 or 0)),
+	alternativeTitles=(map(elem:get(meta_offset    ):select("a"), text)),
+	authors          =(map(elem:get(meta_offset + 1):select("a"), text)),
+	genres           =(map(elem:get(meta_offset + 2):select("a"), text)),
+	--sources        =(map(elem:get(meta_offset + 3):select("a"), text)),
+	status   =(NovelStatus(elem:get(meta_offset + 4):select("a"):text() == "Completed" and 1 or 0)),
 	
 	searchListSel = "list.list-novel.col-xs-12",
 	appendURLToInfoImage = false,
