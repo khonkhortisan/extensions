21029debce3dc074934e8bd71a0a2ba396f69d70
Kyle Mills <Khonkhortisan@gmail.com>
2021-11-06 16:36:58 -0700

    data[PAGE] off by one?

--- src/es/NovelasLigerasnet.lua
+++ src/es/NovelasLigerasnet.lua
@@ -1,4 +1,4 @@
--- {"id":28505740,"ver":"1.0.4","libVer":"1.0.0","author":"Khonkhortisan","dep":["url>=1.0.0","CommonCSS>=1.0.0"]}
+-- {"id":28505740,"ver":"1.0.5","libVer":"1.0.0","author":"Khonkhortisan","dep":["url>=1.0.0","CommonCSS>=1.0.0"]}
 --,"Madara>=2.2.0"]}
 
 local baseURL = "https://www.novelasligeras.net"
@@ -203,7 +203,7 @@ end
 local function listing(name, inc, url)
 	url = expandURL(url)
 	return Listing(name, inc, function(data)
-		return parseListing(GETDocument(inc and (url .. "/page/" .. data[PAGE] .. "/") or url))
+		return parseListing(GETDocument(inc and (url .. "/page/" .. data[PAGE]+1 .. "/") or url))
 	end)
 end
 
