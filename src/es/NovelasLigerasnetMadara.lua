ce4051571226d71122ac8d6fb4d9fbe0cd495774
Kyle Mills <Khonkhortisan@gmail.com>
2021-11-05 22:57:08 -0700

    save file

---
+++ src/es/NovelasLigerasnetMadara.lua
@@ -0,0 +1,12 @@
+-- {"id":28505741,"ver":"1.0.0","libVer":"1.0.0","author":"Khonkhortisan","dep":["Madara>=2.2.0"]}
+
+
+return Require("Madara")("https://www.novelasligeras.net", {
+	id = 28505741,
+	name = "NovelasLigerasnetMadara",
+	imageURL = "https://github.com/shosetsuorg/extensions/raw/dev/icons/NovelasLigeras.png",
+	imageURL = "https://github.com/khonkhortisan/extensions/raw/novelasligeras.net/icons/NovelasLigeras.png", --TODO
+	
+	--ORDER_BY_FILTER_EXT = { "Relevance", "Latest", "A-Z", "Rating", "Trending", "Most Views", "New" }
+	
+})
