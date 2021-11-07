93f336c6a96702072ea30fab31bb37974c2d3991
Kyle Mills <Khonkhortisan@gmail.com>
2021-11-07 00:14:19 -0700

    data[PAGE]=1

--- src/es/NovelasLigerasnet.lua
+++ src/es/NovelasLigerasnet.lua
@@ -1,4 +1,4 @@
--- {"id":28505740,"ver":"1.0.16","libVer":"1.0.0","author":"Khonkhortisan","dep":["url>=1.0.0","CommonCSS>=1.0.0"]}
+-- {"id":28505740,"ver":"1.0.17","libVer":"1.0.0","author":"Khonkhortisan","dep":["url>=1.0.0","CommonCSS>=1.0.0"]}
 --,"Madara>=2.2.0"]}
 
 local baseURL = "https://www.novelasligeras.net"
@@ -204,7 +204,10 @@ end
 local function listing(name, inc, url)
 	url = expandURL(url)
 	return Listing(name, inc, function(data)
-		return parseListing(GETDocument(inc and (url .. "/page/" .. data[PAGE]+1 .. "/") or url))
+		if data[PAGE] == 0 then
+			data[PAGE] = 1
+		end
+		return parseListing(GETDocument(inc and (url .. "/page/" .. data[PAGE] .. "/") or url))
 	end)
 end
 
