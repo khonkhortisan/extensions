f49f6ac132e2b98fc57123dbc49e2f3638b7e150
Kyle Mills <Khonkhortisan@gmail.com>
2021-11-08 02:21:21 -0800

    no www needed

--- src/es/NovelasLigerasnet.lua
+++ src/es/NovelasLigerasnet.lua
@@ -1,7 +1,9 @@
--- {"id":28505740,"ver":"1.0.18","libVer":"1.0.0","author":"Khonkhortisan","dep":["url>=1.0.0","CommonCSS>=1.0.0"]}
+-- {"id":28505740,"ver":"1.0.19","libVer":"1.0.0","author":"Khonkhortisan","dep":["url>=1.0.0","CommonCSS>=1.0.0"]}
 --,"Madara>=2.2.0"]}
 
-local baseURL = "https://www.novelasligeras.net"
+--WordPress site, plugins: WooCommerce, Yoast SEO, js_composer, user_verificat_front, avatar-privacy
+
+local baseURL = "https://novelasligeras.net"
 
 local CATEGORIAS_FILTER_INT = {
 	[0] =""  , --Cualquier Categoría
@@ -358,7 +360,7 @@ return {
 	search = function(data)
 		--local url = self.createSearchString(data)
 		--local url = baseURL .. "/" .. "index.php/lista-de-novela-ligera-novela-web" .. "/"""?s=" .. encode(query)
-		local url = "https://www.novelasligeras.net/?s="..encode(data[QUERY]).."&post_type=product&title=1&excerpt=1&content=0&categories=1&attributes=1&tags=1&sku=0&orderby=title-DESC&ixwps=1"
+		local url = "https://novelasligeras.net/?s="..encode(data[QUERY]).."&post_type=product&title=1&excerpt=1&content=0&categories=1&attributes=1&tags=1&sku=0&orderby=title-DESC&ixwps=1"
 		--return self.parseListing(GETDocument(url), true)
 		return self.parseListing(GETDocument(url))
 	end,
