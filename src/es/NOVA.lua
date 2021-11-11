a43e57037ab9378c715effa41c1a8939d2739293
Kyle Mills <Khonkhortisan@gmail.com>
2021-11-11 05:44:41 -0800

    show tags on novels, to filter with

--- src/es/NOVA.lua
+++ src/es/NOVA.lua
@@ -1,4 +1,4 @@
--- {"id":28505740,"ver":"1.0.42","libVer":"1.0.0","author":"Khonkhortisan","dep":["url>=1.0.0","CommonCSS>=1.0.0"]}
+-- {"id":28505740,"ver":"1.0.43","libVer":"1.0.0","author":"Khonkhortisan","dep":["url>=1.0.0","CommonCSS>=1.0.0"]}
 
 local baseURL = "https://novelasligeras.net" --WordPress site, plugins: WooCommerce, Yoast SEO, js_composer, user_verificat_front, avatar-privacy
 
@@ -242,7 +242,8 @@ return {
 			status = status,
 			genres = map(genres:select("a"), text),
 			tags = map(tags:select("a"), text),
-			description = page:selectFirst(".woocommerce-product-details__short-description"):text(),
+			description = page:selectFirst(".woocommerce-product-details__short-description"):text()..
+				"<br>Etiquetas: "..tostring(tags),
 		}
 		-- '.wpb_wrapper' has left column whole chapters '.wpb_tabs_nav a' and right column chapter parts '.post-content a'
 		if loadChapters then
